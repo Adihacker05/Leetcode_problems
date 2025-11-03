52cb3ab143cfdc58d4ea84f3ddfa220b3a9d2ab2
Adihacker05 <146056804+Adihacker05@users.noreply.github.com>
2025-11-03 21:57:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        int n=nums.length;
+        int temp=0;
+        for(int j=0;j<n;j++)
+        {
+            if(nums[j]==0)
+            {
+                i=j;
+                break;
+            }
+        }
+        for(int k=i+1;k<n;k++)
+        {
+            if(nums[k]!=0 && nums[k]==0)
+            {
+                temp=nums[k];
+                nums[k]=nums[i];
+                nums[i]=temp;
+                i++;
+            }
+        }
+    }
+}
