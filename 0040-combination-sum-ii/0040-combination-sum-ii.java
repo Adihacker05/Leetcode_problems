7af14b2c65477b336b1223ebf5b28ddd253ea1ed
Aditya Kumar <146056804+Adihacker05@users.noreply.github.com>
2026-09-11 22:25:35 +0530

    Time: 5 ms (99.08%), Space: 45.6 MB (19.13%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void findcomb(int ind,int []arr,int target,List<List<Integer>> ans, List<Integer> ds)
+    {
+        if(target==0)
+        {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=ind ; i<arr.length;i++)
+        {
+            if(i>ind  && arr[i]==arr[i-1])
+            {
+                continue;
+            }
+            if(arr[i]>target)
+            {
+                break;
+            }
+            ds.add(arr[i]);
+            findcomb(i+1,arr,target-arr[i],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+    public List<List<Integer>> combinationSum2(int[] cand, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(cand);
+        findcomb(0,cand,target,ans,new ArrayList<>());
+        return ans;
+    }
+}
